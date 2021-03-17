deb3dcf195e92a3e172fe7a18493878d607a7c45
rounakdey <rounakdey@gmail.com>
2021-03-17 14:57:03 -0400

    Wrapper for the full pipeline added
    The entire pipeline can be run with one wrapper function now.

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 Efficiently calculate ancestry-adjusted sparse GRM
 
 ## Example pipeline
+There are two ways of running the analysis pipeline.
+
+### Option 1. Run the pipeline step-by-step.
+This gives the user more control over memory and CPU usage in each step. This option is suggested for analyzing very large dataset.
+
 Step 1. Run KING (version >=2.1.6) - KING suggests not using pruned SNPs
 
-    king -b <bedfile> --ibdseg --degree 4 --cpus <n_cpus> --prefix <output.king>
+    king -b <unfiltered.bedfile> --ibdseg --degree 4 --cpus <n_cpus> --prefix <output.king>
 
 Step 2. Get ancestry divergence estimates - From this step onwards, one can use pruned SNPs to save computation costs
 
@@ -16,9 +21,21 @@ Step 3. Extract unrelated samples
 
 Step 4. Run PCA
 
-    R  CMD BATCH --vanilla '--args --prefix.in <prefix.bedfile> --file.unrels <output.unrelated> --prefix.out <output.pca> --no_pcs 20 --num_threads 30' runPCA_wrapper.R runPCA.Rout
+    R  CMD BATCH --vanilla '--args --prefix.in <prefix.bedfile> --file.unrels <output.unrelated> --prefix.out <output.pca> --no_pcs 20 --num_threads <n_cpus>' runPCA_wrapper.R runPCA.Rout
 
 Step 5. Calculate Sparse GRM
 
-    R CMD BATCH --vanilla '--args --prefix.in <prefix.bedfile> --prefix.out <output.sparseGRM> --file.train <output.unrelated> --file.score <output.pca.score> --file.seg <output.king> --block.size 50000 --num_threads 30 --no_pcs 20' calcSparseGRM_wrapper.R calcSparseGRM.Rout
+    R CMD BATCH --vanilla '--args --prefix.in <prefix.bedfile> --prefix.out <output.sparseGRM> --file.train <output.unrelated> --file.score <output.pca.score> --file.seg <output.king> --num_threads <n_cpus> --no_pcs 20' calcSparseGRM_wrapper.R calcSparseGRM.Rout
 
+### Option 2. Run the entire pipeline with one wrapper function.
+The user has to specify the same memory and CPU threads for all the steps. This option is not suggested for very large dataset.
+
+    R CMD BATCH --vanilla '--args --prefix.in <prefix.bedfile> --prefix.in.unfiltered <prefix.unfiltered.bedfile> --prefix.out <output.sparseGRM> --KING.executable <king.executable.path> --degree 4 --num_threads <n_cpus> --no_pcs 20' runPipeline_wrapper.R runPipeline.Rout
+
+### Details about the arguments
++ *prefix.bedfile*: Prefix of the BED file used for calculating PC scores and Sparse GRM. This file can be pruned and MAF filtered (suggested to use MAF>0.05 and LD pruned variants).
++ *prefix.unfiltered.bedfile* (*unfiltered.bedfile*): Prefix (full path) of the BED file used for running KING IBD segment analysis. No MAF filtering or pruning is required (and suggested as well) for this file. No not need to specify *prefix.unfiltered.bedfile* in Option 2 if you want to use the same file for both running KING and PCA/Sparse GRM calculation.
++ *n_cpus*: Number of CPU threads to use.
++ *king.executable.path*: Full path to the KING executable file.
++ *output.king/output.divergence/output.unrelated/output.pca*: Intermediate outputs for each of the steps.
++ *output.sparseGRM*: Prefix of the final sparse GRM output file.
---
+++ extdata/runPipeline_wrapper.R
@@ -0,0 +1,97 @@
+library('optparse')
+
+options(stringsAsFactors=F)
+
+
+option_list <- list(
+  make_option("--prefix.in", type="character", default="",
+    help="Prefix to the input PLINK bed file used for PCA and SparseGRM calculation (it should be accompanied by a fam and a bim file with the same prefix). Pruned and MAF filtered bed files are preferable."),
+  make_option("--prefix.in.unfiltered", type="character", default="",
+    help="Prefix to the input PLINK bed file used for KING (it should be accompanied by a fam and a bim file with the same prefix). To run KING, pruning and MAF filtering are not needed. If not provided, then prefix.in will be used."),
+  make_option("--KING.executable", type="character", default="king",
+    help="Address to the KING executable file."),
+  make_option("--num_threads", type="integer", default=0,
+    help="Number of threads within each core to use (if 0, then automatically calculated)"),
+  make_option("--degree", type="integer", default=4,
+    help="Consider samples related up to which degree (max 4, default 4)"),
+  make_option("--divThresh", type="numeric", default=-0.02209709,
+    help="Threshold for calculating ancestry divergence among unrelated samples (default -0.02209709)"),
+  make_option("--nRandomSNPs", type="integer", default=0,
+    help="Number of random SNPs to calculate ancestry divergence (default 0, which uses all available SNPs in the plink file)"),
+  make_option("--file.include", type="character", default="",
+    help="IDs for samples to always include in the unrelated set. The first column should be FID and second column should be IID. If only one column present, then that will be treated as both FID and IID"),
+  make_option("--no_pcs", type="integer", default=10,
+    help="Number of PCs to calculate and adjust (default 10)"),
+  make_option("--no_iter", type="integer", default=10,
+    help="Number of iterations for randomized PCA (default 10)"),
+  make_option("--block.size", type="integer", default=5000,
+    help="SNP block size to load in memory"),
+  make_option("--max.related.block", type="integer", default=5000,
+    help="Maximum allowed size of a related sample block"),
+  make_option("--tempDir", type="character", default="",
+    help="Directory to store intermediate files. If not provided, then default temporary directory of the system will be used."),
+  make_option("--deleteTemp", type="logical", default=TRUE,
+    help="Whether to delete the temporary files or not."),
+  make_option("--prefix.out", type="character", default="",
+    help="Prefix to the output file")
+)
+
+parser <- OptionParser(usage="%prog [options]", option_list=option_list)
+
+args <- parse_args(parser, positional_arguments = 0)
+opt <- args$options
+print(opt)
+
+prefix.in <- opt$prefix.in
+prefix.in.unfiltered <- opt$prefix.in.unfiltered
+KING.executable <- opt$KING.executable
+num_threads <- opt$num_threads
+degree <- opt$degree
+divThresh <- opt$divThresh
+nRandomSNPs <- opt$nRandomSNPs
+file.include <- opt$file.include
+no_pcs <- opt$no_pcs
+no_iter <- opt$no_iter
+block.size <- opt$block.size
+max.related.block <- opt$max.related.block
+tempDir <- opt$tempDir
+deleteTemp <- opt$deleteTemp
+prefix.out <- opt$prefix.out
+
+
+getPCadjSparseGRM<-function(prefix.in,prefix.in.unfiltered,KING.executable,num_threads,degree,divThresh,nRandomSNPs,file.include,no_pcs,no_iter,block.size,max.related.block,tempDir,deleteTemp,prefix.out)
+{
+	library('FastSparseGRM')
+
+	if(prefix.in.unfiltered=="")	prefix.in.unfiltered<-prefix.in
+	if(tempDir=="")	tempDir<-tempdir()
+	prefix.out.basename<-basename(prefix.out)
+	prefix.temp.out<-paste0(tempDir,"/",prefix.out.basename)
+
+	print("Running KING IBD segment analysis")
+	system(paste0(KING.executable," -b ",prefix.in.unfiltered,".bed --ibdseg --degree 4 --cpus ",num_threads," --prefix ",prefix.out=prefix.temp.out))
+
+	print("Calculating ancestry divergence")
+	getDivergence(file.seg=paste0(prefix.temp.out,".seg"),prefix.in,num_threads,divThresh,degree,nRandomSNPs,prefix.temp.out)
+
+	print("Extracting unrelated samples")
+	extractUnrelated(file.seg=paste0(prefix.temp.out,".seg"),file.div=paste0(prefix.temp.out,".div"),prefix.in,degree,file.include,prefix.out=prefix.temp.out)
+
+	print("Calculating PC scores")
+	runPCA(prefix.in,file.unrels=paste0(prefix.temp.out,".unrels"),no_pcs,num_threads,prefix.out,no_iter)
+
+	print("Calculating Sparse GRM")
+	calcSparseGRM(prefix.in,file.score=paste0(prefix.out,".score"),file.train=paste0(prefix.temp.out,".unrels"),file.seg=paste0(prefix.temp.out,".seg"),no_pcs,num_threads,prefix.out,degree,block.size,max.related.block)
+
+	if(deleteTemp==TRUE)
+	{
+		print("Cleaning up intermediate files")
+		unlink(paste0(prefix.temp.out,c(".seg",".div",".unrels",".segments.gz","allsegs.txt")))
+		unlink(paste0(prefix.out,c(".evec",".eval")))
+	}
+
+}
+
+
+getPCadjSparseGRM(prefix.in,prefix.in.unfiltered,KING.executable,num_threads,degree,divThresh,nRandomSNPs,file.include,no_pcs,no_iter,block.size,max.related.block,tempDir,deleteTemp,prefix.out)
+
